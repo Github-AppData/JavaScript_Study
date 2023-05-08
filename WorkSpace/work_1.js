5c3b2c4f2f61cf899ffde523e9fc9c7953acfbf6
Github-AppData <77886661+Github-AppData@users.noreply.github.com>
2023-05-08 11:49:57 +0900

    First commit

---
+++ WorkSpace/work_1.js
@@ -0,0 +1,10 @@
+// 1. 변수
+// 꿀팁 : 일단 모두 다 const로 표현 한 뒤, 변할 가능성이 있는 것만, let으로 바꾸어주라.
+
+// let - 변하는 값 / const - 상수
+let name = 'Mike';
+const PI = 3.14;
+
+// 출력 방식 2가지
+console.log(name + " : " + PI);
+alert(name);
---
+++ WorkSpace/work_2.js
@@ -0,0 +1,58 @@
+// 자료형
+
+// 문자열 String
+// const name = "Mike"; 
+// const age = 30;
+
+// const message = "I'm a boy";
+// const message2 = "I\'m a boy";
+
+// const message3 = `My name is ${name}`;
+// const message4 = `나는 ${30+10}살 입니다.`;
+
+// console.log(message4);
+
+
+// 숫자형
+// const x = 1/0;
+// console.log(x);
+
+// const name = "Mike";
+// const y = name/2;
+
+// console.log(y);
+// NaN = Not a Number
+
+
+// boolean
+// const a = true;
+// const b = false;
+
+// const name = "Mike";
+// const age = 30;
+
+// console.log(name == 'mike')
+// console.log(age < 50)
+
+
+// Null 과 Undefined
+// Null은 값이 없음을 뜻하고, Undefined는 값이 할당되지 않았음을 뜻한다.
+
+// let age;
+// console.log(age);
+
+// let user = null;
+// console.log(user);
+
+
+// typeof 연산자
+// 변수의 자료형을 알 수 있다.
+
+const name = "Mike";
+
+console.log(typeof 3);
+console.log(typeof name);
+console.log(typeof true);
+console.log(typeof "xxxx");
+console.log(typeof null); // 객체형 - null은 객체가 아니다.
+console.log(typeof undefined);
